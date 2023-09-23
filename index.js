5682c7aade2ad508daefd7437ac6d6a2f6ae8fff
Pratham Choudhary <prathamchoudhary924@gmail.com>
2023-09-23 10:20:40 +0530

    body-parser updated

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000;
 const dbConnect = require("./config/database");
 const operations = require("./routes/operations");
 require("dotenv").config();
 
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use("/api/to-do",operations);
 app.listen(PORT,()=>{
